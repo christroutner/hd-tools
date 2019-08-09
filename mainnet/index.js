e0ee906508b4e8e7d09cac403517d3f094bd9d81
Chris Troutner <chris.troutner@gmail.com>
2019-08-09 14:28:33 -0700

    First rough prototype

--- mainnet/index.js
+++ mainnet/index.js
@@ -6,4 +6,4 @@
 const Blockbook = require('./lib/blockbook')
 const blockbook = new Blockbook()
 
-blockbook.hello()
+blockbook.backup()
--- mainnet/lib/blockbook.js
+++ mainnet/lib/blockbook.js
@@ -3,11 +3,46 @@
 
 'use strict'
 
+const shell = require("shelljs");
+
+const WORK_DIR = `/mnt/usb/indexers/blockbook/mainnet`
+
 class Blockbook {
   constructor() {}
 
   hello() {
     console.log(`hello world`)
+  }
+
+  backup() {
+    try {
+      // Enter to the working directory.
+      shell.cd(WORK_DIR)
+
+      // Stop Docker container
+      shell.exec(`docker stop blockbook`)
+      console.log(`Blockbook Docker container stopped.`)
+
+      // Move old data
+      shell.mv(`*.zip`, `old-data/`)
+      console.log(`Old zip data moved.`)
+
+      // Zip the data folder.
+      console.log(`Zipping data...`)
+      shell.exec(`zip -r blockbook-data.zip data/`)
+      console.log(`...Finished zipping data.`)
+
+      // Restart the Docker container
+      console.log(`Starting Docker container.`)
+      shell.cd(`docker-ubuntu-blockbook`)
+      shell.exec(`docker-compose up -d`)
+      console.log(`Docker container started.`)
+
+      // Move the old data.
+
+    } catch(err) {
+      console.log(`Error in blockbook.js/blockbook(): `, err);
+    }
   }
 }
 
