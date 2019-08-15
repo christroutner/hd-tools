2026770e6d677541814b383bbe6a2ad62ca4114d
Chris Troutner <chris.troutner@gmail.com>
2019-08-15 08:23:07 -0700

    Adding full node backup script

---
+++ mainnet/lib/abc-full-node.js
@@ -0,0 +1,46 @@
+/*
+  This library controls the backup of ABC full node data.
+*/
+
+'use strict'
+
+const shell = require("shelljs");
+
+const WORK_DIR = `/mnt/usb/full-nodes/mainnet`
+
+class ABC {
+  constructor() {}
+
+  async backup() {
+    try {
+      console.log(`Starting abc-full-node.js/backup()`)
+
+      // Enter to the working directory.
+      shell.cd(WORK_DIR)
+
+      // Move old data
+      shell.mv(`*.zip`, `old-data/`)
+      console.log(`Old zip data moved.`)
+
+      // Zip the data folder.
+      console.log(`Zipping data...`)
+      //shell.exec(`zip -r abc-full-node-mainnet-data.zip blockchain-data/`)
+      console.log(`...Finished zipping data.`)
+
+      // Delete the old data.
+      shell.rm(`rm ./old-data/*.zip`)
+      console.log(`Deleted old data`)
+
+      console.log(`Finished abc-full-node.js/backup()`)
+
+    } catch(err) {
+      console.log(`Error in abc-full-node.js/backup(): `, err);
+    }
+  }
+
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+}
+
+module.exports = ABC
---
+++ mainnet/scripts/bkup-abc-full-node.js
@@ -0,0 +1,33 @@
+/*
+  This is the main application that kicks off backup tasks.
+*/
+
+'use strict'
+
+const Containers = require('../lib/mainnet-containers')
+const containers = new Containers()
+
+const ABC = require('../lib/abc-full-node')
+const abc = new ABC()
+
+async function startBackup() {
+  try {
+    console.log(`Stopping all Docker containers: ${timestamp()}`)
+    await containers.stop()
+
+    await abc.backup()
+    console.log(`Finished backing up ABC blockchain: ${timestamp()}`)
+
+    await containers.start()
+    console.log(`All Docker containers started: ${timestamp()}`)
+
+  } catch(err) {
+    console.log(`Error in startBackup(): `, err)
+  }
+}
+startBackup()
+
+function timestamp() {
+  const now = new Date()
+  return now.toLocaleString()
+}
